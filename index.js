a71e2b6e422c019bc9ea14afc5afe6eae4b8be0a
Miguelslo27 <miguel.sosa@globant.com>
2018-11-13 19:15:51 -0300

    feature/secure-server - Added Hemlet to secure the app basics, installed some dependencies

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const helmet = require("helmet");
 const app = express();
 const port = process.env.PORT || 3200;
+
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.send("Server is online and waiting...");
