6b322c0caabdea2b1f6e9cc2f6ba99fae24537e3
Miguelslo27 <miguel.sosa@globant.com>
2018-11-13 18:33:36 -0300

    feature/webservice-state - Added environment port to deploy and get it to work

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3200;
 
 app.get("/", (req, res) => {
   res.send("Server is online and waiting...");
 });
 
-app.listen(3200);
+app.listen(port);
