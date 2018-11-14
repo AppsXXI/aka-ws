6750086223e4fc69071ca69b4becb2e5f189af69
Miguelslo27 <miguel.sosa@globant.com>
2018-11-14 19:07:33 -0300

    poc/database-users-api - Refactor on mongo connection url and cleaned code

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,16 +1,7 @@
 const mongoose = require("mongoose");
+const mongoUsr = process.env.MONGO_USR || 'dev-admin-0001';
+const mongoPss = process.env.MONGO_PSS || 'd3v-4dm1n-oooi';
+const mongoDb = process.env.MONGO_DB || 'test';
+const mongoUrl = `mongodb+srv://${mongoUsr}:${mongoPss}@cluster0-9tphu.mongodb.net/${mongoDb}?retryWrites=true`;
 
-// MONGO DB TEST
-// var MongoClient = require('mongodb').MongoClient;
-
-var uri = "mongodb+srv://dev-admin-0001:d3v-4dm1n-oooi@cluster0-9tphu.mongodb.net/test?retryWrites=true";
-
-// MongoClient.connect(uri, function(err, client) {
-//    const collection = client.db("test").collection("devices");
-//    // perform actions on the collection object
-//    client.close();
-// });
-// ENDs MONGO DB TEST
-
-
-mongoose.connect(uri);
+mongoose.connect(mongoUrl);
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const helmet = require("helmet");
+// DataBase
 const db = require("./db/dbconnect");
+// User Controller
 const UserController = require("./user/UserController");
 
 const app = express();
 const port = process.env.PORT || 3200;
+const apiBase = '/api';
 
 app.use(helmet());
-app.use('/users', UserController);
+app.use(`${apiBase}/users`, UserController);
 
+console.log("Server starting...");
 app.get("/", (req, res) => {
+  console.log("Server is started!");
   res.send("Server is online and waiting...");
 });
 
