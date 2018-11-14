cfac3b4fe10285a5fa2c768d350ad2300e9e8c3a
Miguelslo27 <miguel.sosa@globant.com>
2018-11-14 19:42:56 -0300

    poc/database-users-api - Refactor - deleted comments

--- user/UserController.js
+++ user/UserController.js
@@ -8,10 +8,6 @@ router.use(bodyParser.json());
 const User = require("./User");
 
 router.post("/", (req, res) => {
-  console.log(req.body.name);
-  console.log(req.body.email);
-  console.log(req.body.password);
-  
   User.create({
     name: req.body.name,
     email: req.body.email,
@@ -32,6 +28,40 @@ router.get("/", (req, res) => {
     }
 
     res.status(200).send(users);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  User.findById(req.params.id, (error, user) => {
+    if (error) {
+      return res.status(500).send("There was a problem getting the user.");
+    }
+
+    if (!user) {
+      return res.status(404).send("User not found.");
+    }
+
+    res.status(200).send(user);
+  });
+});
+
+router.put("/:id", (req, res) => {
+  User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, user) => {
+    if (error) {
+      return res.status(500).send("There was a problem updating the user.");
+    }
+
+    res.status(200).send(user);
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  User.findByIdAndRemove(req.params.id, (error, user) => {
+    if (error) {
+      return res.status(500).send("There was a problem deleting the user.");
+    }
+
+    res.status(200).send(`User ${user.name} was deleted successful.`);
   });
 });
 
