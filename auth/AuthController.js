9e7316bdd1b88e43de3b87ed2a21b844349d9815
Miguelslo27 <miguel.sosa@globant.com>
2018-11-15 10:13:14 -0300

    poc/authentication-key - Added login and logout routes

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -27,6 +27,28 @@ router.post("/register", (req, res) => {
   });
 });
 
+router.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (error, user) => {
+    if (error) {
+      return res.status(500).send("There was a server error.");
+    }
+
+    if (!user) {
+      return res.status(404).send("No user found.");
+    }
+
+    const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+    if (!passwordIsValid) {
+      return res.status(401).send({ auth: false, token: null });
+    }
+
+    const token = jwt.sign({ id: user._id }, config.secret, { expiresIn: 86400 });
+
+    res.status(200).send({ auth: true, token: token });
+  });
+});
+
 router.post("/authenticate", (req, res) => {
   const token = req.headers["x-access-token"];
 
@@ -39,7 +61,7 @@ router.post("/authenticate", (req, res) => {
       return res.status(500).send({ auth: false, message: "Failed to authenticate token." });
     }
 
-    User.findById(decoded.id, (error, user) => {
+    User.findById(decoded.id, { password: 0 }, (error, user) => {
       if (error) {
         return res.status(500).send("There was a problem finding the user.");
       }
@@ -51,6 +73,10 @@ router.post("/authenticate", (req, res) => {
       res.status(200).send(user);
     });
   });
+});
+
+router.get("/logout", (req, res) => {
+  res.status(200).send({ auth: false, token: null });
 });
 
 module.exports = router;
